0d9feeef4ff7246a985ee8f7d8acd70d558b9842
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-07 17:31:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,9 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  
+  if hash[key]
+    hash[key]
+  end
 end
 
 def update_counting_hash(hash, key)
